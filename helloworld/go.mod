daa3c6641e53230a63198342bd03ce5ff650249e
Maurizio Brioschi <maurizio.brioschi@gmail.com>
2020-03-29 19:47:19 +0200

    add external module

--- helloworld/go.mod
+++ helloworld/go.mod
@@ -1,3 +1,5 @@
 module LearnGo/helloworld
 
 go 1.14
+
+require github.com/google/go-cmp v0.4.0
--- helloworld/helloworld.go
+++ helloworld/helloworld.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"LearnGo/helloworld/morestrings"
+	"github.com/google/go-cmp/cmp"
 )
 
 func main() {
 	// moreStrings has to be built before importing, only functions starting in upper case are exported
 	fmt.Println(morestrings.ReverseRunes("ciao"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
